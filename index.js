aeac30fe089607420e782288ab2d8fefb1eea75a
Selaka Nanayakkara <selaka.kithmal@gmail.com>
2021-09-24 23:53:47 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const menu = require('./routes/menu');
+const app = express()
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use('/menu', menu)
+module.exports = app
